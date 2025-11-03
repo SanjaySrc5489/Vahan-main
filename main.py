f7a565f3edf59026973820023a384baa0cb45147
SanjaySrc5489 <133635183+SanjaySrc5489@users.noreply.github.com>
2025-11-04 04:25:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -151,5 +151,5 @@ def vahan_api():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
