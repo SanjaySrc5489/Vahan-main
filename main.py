760532bda01a7f0aa879736d4e03b6d9e9906255
SanjaySrc5489 <133635183+SanjaySrc5489@users.noreply.github.com>
2025-11-04 04:33:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -87,14 +87,21 @@ def click_proceed_button_in_dialog(driver):
                 return True
     return False
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
 def create_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.binary_location = "/opt/chrome/chrome"
+
     service = Service("/usr/local/bin/chromedriver")
     return webdriver.Chrome(service=service, options=chrome_options)
 
