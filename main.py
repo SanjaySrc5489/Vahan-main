45ce6fe8a548161352a1ac989cc568cd0cc1d3b8
SanjaySrc5489 <133635183+SanjaySrc5489@users.noreply.github.com>
2025-11-04 04:06:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -150,4 +150,6 @@ def vahan_api():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6464)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
